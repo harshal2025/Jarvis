84ea70f24b592b61a6c5e97b9727769d9519143d
harshal2025 <harshalporwal1711@gmail.com>
2025-05-19 16:33:41 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,16 @@
+music = {
+    "die with a smile": "https://www.youtube.com/watch?v=kPa7bsKwL-c",
+    "blue": "https://www.youtube.com/watch?v=IpFX2vq8HKw",
+    "golden hour": "https://www.youtube.com/watch?v=PEM0Vs8jf1w",
+    "until i found you": "https://www.youtube.com/watch?v=GxldQ9eX2wo",
+    "7 years": "https://www.youtube.com/watch?v=LHCob76kigA",
+    "another love": "https://www.youtube.com/watch?v=MwpMEbgC7DA",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "night changes": "https://www.youtube.com/watch?v=syFZfO_wfMQ",
+    "forever young": "https://www.youtube.com/watch?v=YHRvDo8rUoQ",
+    "memory reboot": "https://www.youtube.com/watch?v=wL8DVHuWI7Y",
+    "let her go x husn": "https://www.youtube.com/watch?v=bL6dJjxm0x0",
+    "beautiful things": "https://www.youtube.com/watch?v=Oa_RSwwpPaA",
+    "fein": "https://www.youtube.com/watch?v=B9synWjqBn8",
+    "end of beginning": "https://www.youtube.com/watch?v=xy3AcmW0lrQ"
+}
